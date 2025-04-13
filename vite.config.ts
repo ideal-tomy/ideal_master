de78ea113214a867c2bb47a2d9c1ddc5073eaa65
kamibukuro18 <39715399+kamibukuro18@users.noreply.github.com>
2025-04-14 01:29:32 +0900

    pages用修正

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import type { ProxyOptions } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Netlifyデプロイのため、ルートパスに設定
+  base: '/ideal_master/', // GitHub Pagesデプロイ用にリポジトリ名を指定
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
