35875d589eaf6370c2be7450ad3064fc5ba0da45
ideal-tomy <ryojitomii@gmail.com>
2025-04-15 02:14:03 +0900

    feat: simplify HomePage for testing loop issue

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react'
-import { motion } from 'framer-motion'
+import { Heading } from '@chakra-ui/react'
 
-// ホームページ用コンポーネントのインポート
-import Hero from '../components/home/Hero'
-import ServiceHighlights from '../components/home/ServiceHighlights'
-import CompanyValues from '../components/home/CompanyValues'
-import FeaturedCases from '../components/home/FeaturedCases'
-import Testimonials from '../components/home/Testimonials'
-import ContactCTA from '../components/home/ContactCTA'
+// ホームページ用コンポーネントのインポート (コメントアウト)
+// import Hero from '../components/home/Hero'
+// import ServiceHighlights from '../components/home/ServiceHighlights'
+// import CompanyValues from '../components/home/CompanyValues'
+// import FeaturedCases from '../components/home/FeaturedCases'
+// import Testimonials from '../components/home/Testimonials'
+// import ContactCTA from '../components/home/ContactCTA'
 
 const HomePage: React.FC = () => {
   // ページ読み込み時にスクロール位置をトップに設定
@@ -16,31 +16,7 @@ const HomePage: React.FC = () => {
   }, [])
 
   return (
-    <>
-      {/* ヒーローセクション */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      >
-        <Hero />
-      </motion.div>
-
-      {/* サービスハイライト */}
-      <ServiceHighlights />
-
-      {/* 会社の価値提案 */}
-      <CompanyValues />
-
-      {/* 主要実績（事例紹介） - 新しく実装したもの */}
-      <FeaturedCases isHomePage={true} />
-
-      {/* お客様の声 */}
-      <Testimonials />
-
-      {/* お問い合わせCTA */}
-      <ContactCTA />
-    </>
+    <Heading>Home Page Test</Heading>
   )
 }
 
